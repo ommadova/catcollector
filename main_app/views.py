489b3c94e1d88fd4f126820398dbac707f98a28d
ommadova <ommadovaa67@gmail.com>
2025-06-27 11:50:42 -0500

    Adding static

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'A very cute cat', 2),  
+    Cat('Momo', 'Siamese', 'A very fluffy cat', 3),
+    Cat('Kiki', 'Persian', 'A very lazy cat', 5),
+    Cat('Neko', 'Bengal', 'A very playful cat', 1),
+]
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html', {})
 
 def about (request):
-    return HttpResponse('<h1>About the Cat Collector</h1>')
+    return render(request, 'about.html')
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
+
