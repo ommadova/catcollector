6ced9d79fef09d8f5cd34cab1d6db0cfdacbe2a0
ommadova <ommadovaa67@gmail.com>
2025-07-08 11:08:50 -0500

    Finish CatCollector  part4

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
-
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'A very cute cat', 2),  
-    Cat('Momo', 'Siamese', 'A very fluffy cat', 3),
-    Cat('Kiki', 'Persian', 'A very lazy cat', 5),
-    Cat('Neko', 'Bengal', 'A very playful cat', 1),
-]
 
 # Create your views here.
 
@@ -26,5 +12,23 @@ def about (request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+      model = Cat
+      fields = ['name', 'breed', 'description', 'age']
+    #   success_url = '/cats/{cat.id}/'  # Redirect to the detail page of the newly created cat
+    
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
