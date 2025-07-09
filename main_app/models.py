d03562afce0c52a5750197bebda5dd51df8a3dba
ommadova <ommadovaa67@gmail.com>
2025-07-09 01:02:26 -0500

    Finish catcollector part5

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,42 @@
+import datetime
 from django.db import models
 from django.urls import reverse
 
 # Create your models here.
 
-class Cat(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
-    
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-    
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 
-    def get_absolute_url(self):
-        return reverse('cat-detail', kwargs={'cat_id': self.id})
+class Cat(models.Model):
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return f'{self.name} ({self.id})'
+  
+  def get_absolute_url(self):
+    return reverse('cat-detail', kwargs={'cat_id': self.id})
+  
+  def fed_for_day(self):
+    return self.feeding_set.filter(date=datetime.date.today()).count() >= len(MEALS)
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Will also have a cat_id field on the feeding object
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.get_meal_display()} on {self.date}'
+  
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
-
+from .forms import FeedingForm
 
 # Create your views here.
-
 def home(request):
-    return render(request, 'home.html', {})
+  return render(request, 'home.html')
 
-def about (request):
-    return render(request, 'about.html')
+def about(request):
+  return render(request, 'about.html')
 
 def cat_index(request):
-    cats = Cat.objects.all()
-    return render(request, 'cats/index.html', {'cats': cats})
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', {'cats': cats})
 
 def cat_detail(request, cat_id):
-    cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+  cat = Cat.objects.get(id=cat_id)
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form
+  })
 
 class CatCreate(CreateView):
-      model = Cat
-      fields = ['name', 'breed', 'description', 'age']
-    #   success_url = '/cats/{cat.id}/'  # Redirect to the detail page of the newly created cat
-    
+  model = Cat
+  fields = '__all__'
+  # success_url = '/cats/{id}'
+
 class CatUpdate(UpdateView):
-    model = Cat
-    # Let's disallow the renaming of a cat by excluding the name field!
-    fields = ['breed', 'description', 'age']
+  model = Cat
+  fields = ['breed', 'description', 'age']
 
 class CatDelete(DeleteView):
-    model = Cat
-    success_url = '/cats/'
+  model = Cat
+  success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('cat-detail', cat_id=cat_id)
